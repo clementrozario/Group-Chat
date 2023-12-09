8bb4d07e03501e33cdcee2ce4198ad70a445355b
clementrozario <clementcla@outlook.com>
2023-12-09 18:04:12 +0530

    changes made to generate token for chat in frontend

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,28 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
 exports.authenticate = (req, res, next) => {
     try {
         const token = req.header('Authorization');
-        const user = jwt.verify(token, 'secret')
+        if (!token) {
+            return res.status(401).json({ success: false, message: 'Token not provided' });
+        }
+
+        const user = jwt.verify(token, 'secret');
         User.findByPk(user.userId)
-        .then(user => {
-            // console.log(JSON.stringify(user));
-            req.user = user;
-            next();
-        }).catch (err => {
-            console.log(err)})
-    } catch(err) {
+            .then(user => {
+                if (!user) {
+                    return res.status(401).json({ success: false, message: 'User not found' });
+                }
+                req.user = user;
+                next();
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ success: false, message: 'Internal Server Error' });
+            });
+    } catch (err) {
         console.log(err);
-        return res.status(401).json({success: false})
+        return res.status(401).json({ success: false, message: 'Invalid token' });
     }
-}
+};
